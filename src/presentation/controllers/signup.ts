260c1e59b328fdac9aa626cb82369d4ddab0e489
Leonardo Eichstaedt <leonardoeichs@gmail.com>
2022-07-06 20:23:24 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,7 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
-export class SignupController implements Controller {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
